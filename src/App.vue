50029afc9867b0622be98a916b50e9d4ac5c206f
Elise McCallum <mccallum.ek@gmail.com>
2016-06-06 12:00:09 -0700

    elise - add inital skeleton of add event form

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
-    Hello, World!
+    <h1>Dashboard</h1>
+    <add-event></add-event>
   </div>
 </template>
 <script>
+import AddEvent from './components/AddEvent'
+
+export default {
+  components: {
+    AddEvent
+  }
+}
 </script>
---
+++ src/components/AddEvent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h4>Add Event</h4>
+    <form v-on:submit="addEvent">
+      <fieldset>
+        <label for="event_type">Event Type</label>
+        <input id='event_type' type="text" v-model="eventType"/>
+        <label for="story_number">Story Number</label>
+        <input id='story_number' type="text" v-model="story_number"/>
+        <button>Add Event</button>
+      </fieldset>
+    </form>
+  </div>
+</template>
+<script>
+</script>
