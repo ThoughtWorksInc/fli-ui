0b5d3d68d00c52921ad1066c6f68fff3431e2942
Elise McCallum <mccallum.ek@gmail.com>
2016-06-09 13:21:07 -0700

    elise - add production configuration for deploying the app

---
+++ config/production.js
@@ -0,0 +1,8 @@
+/*
+  This object is aliased as config via webpack
+  The correct file is included via the NODE_ENV variable
+  @usage: import config as 'config'
+*/
+module.exports = {
+  'fliAPI': 'http://fli-change.herokuapp.com/'
+}
