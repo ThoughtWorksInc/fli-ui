d56cdb27fece873f13a2e867868f89d978923231
Elise McCallum <mccallum.ek@gmail.com>
2016-06-09 14:05:15 -0700

    elise - make realistic promise response for fetching groups

--- test/stub-resource.js
+++ test/stub-resource.js
@@ -6,7 +6,19 @@ export function createEvent (eventType, storyNumber) {
 
 export function fetchGroups () {
   return new Promise((resolve, reject) => {
-    resolve()
+    resolve({ response: { data: { 'groups': [
+      {
+        'description': 'all',
+        'sampleSize': 2,
+        'cycleTimeStatistics': {
+          'mean': 1,
+          'median': 1,
+          'standardDeviation': 1,
+          'lowerBound': 1,
+          'upperBound': 1
+        }
+      }
+    ] } } })
   })
 }
 
