7f0f350dbca032d0e472e7f6fa5fb59aa6670449
Elise McCallum <mccallum.ek@gmail.com>
2016-06-08 17:20:08 -0700

    elise/jk - introduce google charts api for cycle time graph component

---
+++ src/AggregateCycleTime.js
@@ -0,0 +1,69 @@
+
+const dataSeries = []
+
+export function drawCycleTime () {
+  var data = defineDataTableColumns()
+  data.addRows(dataSeries)
+  var options = defineChartFormat()
+  var view = new window.google.visualization.DataView(data)
+  var chart = new window.google.visualization.ComboChart(document.getElementById('cycleTime'))
+  chart.draw(view, options)
+}
+
+export function addDataSeries (desc, sampleSize, mean, lowerBound, standardDeviation, upperBound) {
+  var rangeTip = buildRangeTip(lowerBound, upperBound)
+  var description = desc + ' (' + sampleSize + ')'
+  dataSeries.push([description,
+    lowerBound,
+    standardDeviation, rangeTip,
+    standardDeviation, rangeTip,
+    mean, buildAverageTip(mean)])
+}
+
+function defineChartFormat () {
+  return {
+    backgroundColor: 'white',
+    width: 900,
+    height: 600,
+    bar: {groupWidth: '33%'},
+    colors: ['transparent', 'green', 'maroon', 'black'],
+    seriesType: 'bars',
+    series: {3: {type: 'line'}},
+    pointSize: 10,
+    pointShape: 'diamond',
+    lineDashStyle: [0, 1000],  // hack to make the line disapear
+    legend: {position: 'bottom', maxLines: 3},
+    annotations: {
+      textStyle: {
+        fontSize: 10,
+        bold: true,
+        italic: true,
+        color: 'black',     // The color of the text.
+        auraColor: '#d799ae', // The color of the text outline.
+        opacity: 0.8         // The transparency of the text.
+      }
+    },
+    isStacked: true
+  }
+}
+
+function defineDataTableColumns () {
+  var data = new window.google.visualization.DataTable()
+  data.addColumn('string', ' ')
+  data.addColumn('number', ' ')
+  data.addColumn('number', 'Lower Range')
+  data.addColumn({type: 'string', role: 'tooltip'})
+  data.addColumn('number', 'Upper Range')
+  data.addColumn({type: 'string', role: 'tooltip'})
+  data.addColumn('number', 'Average')
+  data.addColumn({type: 'string', role: 'tooltip'})
+  return data
+}
+
+function buildRangeTip (lower, upper) {
+  return 'Stories can be expected to finish between ' + lower + ' and ' + upper + ' days.'
+}
+
+function buildAverageTip (average) {
+  return 'Stories finish on average in ' + average + ' days.'
+}
--- src/components/CycleTimeGraph.vue
+++ src/components/CycleTimeGraph.vue
@@ -1,6 +1,18 @@
 <template>
   <h2> Aggregated Cycle Time </h2>
+  <div id="cycleTime"></div>
 </template>
 
 <script>
+import * as FliGateway from 'src/services/fli-gateway'
+import * as AggregateCycleTime from 'src/AggregateCycleTime'
+
+export default {
+  ready () {
+    FliGateway.fetchGroups().then(groups => {
+      console.log(groups)
+    })
+    window.google.charts.setOnLoadCallback(AggregateCycleTime.drawCycleTime())
+  }
+}
 </script>
