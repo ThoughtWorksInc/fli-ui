547ba9dacfed4aa366f1b43c04e81ee1ebd4c728
Elise McCallum <mccallum.ek@gmail.com>
2016-06-10 12:39:36 -0700

    elise - print list of stories as a list

--- src/components/StoriesList.vue
+++ src/components/StoriesList.vue
@@ -1,7 +1,26 @@
 <template>
   <h2>Story List</h2>
-I will be a list of stories!
+  <div>
+    <ul v-for="story in stories">
+      <li>{{ story.name }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import * as FliGateway from 'src/services/fli-gateway'
+
+export default {
+  data () {
+    return {
+      stories: []
+    }
+  },
+
+  ready () {
+    FliGateway.fetchStories().then(stories => {
+      this.stories = stories
+    })
+  }
+}
 </script>
