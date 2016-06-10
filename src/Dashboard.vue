323af9986f464262678bf07e0699c6bba0e2f90c
Elise McCallum <mccallum.ek@gmail.com>
2016-06-10 11:17:35 -0700

    elise - shell element for a story list, styling to make the graph and list side by side

--- src/Dashboard.vue
+++ src/Dashboard.vue
@@ -2,17 +2,38 @@
   <div id="app">
     <h1>Dashboard</h1>
     <add-event></add-event>
-    <cycle-time-graph></cycle-time-graph>
+    <div>
+      <div class="cycle-time-graph">
+        <cycle-time-graph></cycle-time-graph>
+      </div>
+      <div class="stories-list">
+        <stories-list></stories-list>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import AddEvent from './components/AddEvent'
 import CycleTimeGraph from './components/CycleTimeGraph'
+import StoriesList from './components/StoriesList'
 
 export default {
   components: {
     AddEvent,
-    CycleTimeGraph
+    CycleTimeGraph,
+    StoriesList
   }
 }
 </script>
+
+<style>
+.cycle-time-graph {
+  width: 60%;
+  float: left;
+  display: inline-block;
+}
+
+.stories-list {
+  display: inline-block;
+}
+</style>
---
+++ src/components/StoriesList.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2>Story List</h2>
+I will be a list of stories!
+</template>
+
+<script>
+</script>
