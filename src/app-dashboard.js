53d1a39be52cd6ac0f314b21f161f77824428228
Elise McCallum <mccallum.ek@gmail.com>
2016-06-10 14:53:53 -0700

    elise - add store to the running app

--- src/app-dashboard.js
+++ src/app-dashboard.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import { createRouter } from './router'
+import { createStore } from './vuex/store'
 
 Vue.use(VueResource)
 
@@ -11,10 +12,11 @@ export default class AppDashboard {
 
   start () {
     var router = createRouter()
+    var store = createStore()
     this.router = router
     this.root.appendChild(document.createElement('router-view'))
     return new Promise((resolve, reject) => {
-      router.start({}, this.root, resolve)
+      router.start({store}, this.root, resolve)
     })
   }
 
