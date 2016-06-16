1aac14ca01e542696986d02644764c16cc6ef336
JK Werner <jk@thoughtworks.com>
2016-06-16 12:45:30 -0700

    jk - added tool tip for start and end event

--- src/components/AddEvent.vue
+++ src/components/AddEvent.vue
@@ -4,6 +4,9 @@
       <fieldset>
         <label for="event_type">Event Type</label>
         <input id='event_type' class="event-type-input" type="text" v-model="eventType"/>
+        <div class="tooltip">(?)
+          <span class="tooltiptext">use <i>start</i> to begin a story and <i>end</i> to finish a story</span>
+        </div>
         <label for="story_number">Story Number</label>
         <input id='story_number' class="story-number-input" type="text" v-model="storyNumber"/>
         <button v-on:click="addEvent" class="add-event-button" type="button" role="button">Add Event</button>
@@ -40,3 +43,43 @@ export default {
   }
 }
 </script>
+<style>
+.tooltip {
+   position: relative;
+   display: inline-block;
+   border-bottom: 1px dotted black; /* If you want dots under the hoverable text */
+}
+
+.tooltip .tooltiptext {
+   visibility: hidden;
+   width: 350px;
+   background-color: #555;
+   color: #fff;
+   text-align: center;
+   padding: 5px 0;
+   border-radius: 6px;
+   position: absolute;
+   z-index: 1;
+   bottom: 5px;
+   left: 150%;
+   margin-left: -60px;
+   opacity: 0;
+   transition: opacity 1s;
+}
+
+.tooltip .tooltiptext::after {
+   content: "";
+   position: absolute;
+   top: 100%;
+   left: -90%;
+   margin-left: -5px;
+   border-width: 5px;
+   border-style: solid;
+   border-color: #555 transparent transparent transparent;
+}
+
+.tooltip:hover .tooltiptext {
+   visibility: visible;
+   opacity: 1;
+}
+</style>
